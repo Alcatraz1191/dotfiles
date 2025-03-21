a20ad6c85f2040061c2d0b5d97131bd51b61e0b9
Alcatraz1191 <shrygdkr@gmail.com>
2025-03-21 12:54:47 -0500

    new config

--- fish/.config/fish/config.fish
+++
@@ -1,6 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
-# Created by `pipx` on 2025-02-12 18:12:12
-set PATH $PATH /home/alcatraz1191/.local/bin
--- swww/.config/swww/swww.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-#start swww
-WALLPAPERS_DIR=~/Pictures/Wallpaper/
-#WALLPAPER=$(find "$WALLPAPERS_DIR" -type f | shuf -n 1)
-swww img ~/Pictures/Wallpaper/pixel-castle.png
