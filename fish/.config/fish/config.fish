47fda72dda5d26702f4b9217e019c8dde78a3a8f
Alcatraz1191 <shrygdkr@gmail.com>
2025-03-22 00:12:27 -0500

    more fish changes

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,17 +1,10 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    set fish_greeting
-    source $__fish_config_dir/environment.fish
-    fish_add_path -g ~/.cargo/bin
-    eval $(starship init fish)
-    starship init fish | source
-end
-if status is-login   
-  # Kill any existing agents before starting a new one
-    if not pgrep ssh-agent &>/dev/null
-        eval (keychain)
-    end
-    if not ssh-add -l &>/dev/null
-        ssh-add ~/.ssh/id_ed25519 &>/dev/null
-    end
+  # Commands to run in interactive sessions can go here
+  set fish_greeting
+  source $__fish_config_dir/environment.fish
+  fish_add_path -g ~/.cargo/bin
+  eval $(starship init fish)
+  starship init fish | source
+  #ssh key
+  eval $(keychain --eval --quiet id_ed25519)
 end
