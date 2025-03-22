3419d6d0fb35bd2cc18edb0ed00a5b19487411ac
Alcatraz1191 <shrygdkr@gmail.com>
2025-03-21 23:18:22 -0500

    changes to new theme(in progress)

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    set fish_greeting
     source $__fish_config_dir/environment.fish
     fish_add_path -g ~/.cargo/bin
     eval (ssh-agent -c) >/dev/null
+    eval $(starship init fish)
+    starship init fish | source
 end
