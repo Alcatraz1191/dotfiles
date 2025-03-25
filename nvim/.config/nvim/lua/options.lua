c4d5cfdf6bdee4e8762f719b98998b822de5a3cd
Alcatraz1191 <shrygdkr@gmail.com>
2025-03-25 15:30:36 -0500

    add toggle for autoformat

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -4,15 +4,3 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
-local lspconfig = require "lspconfig"
-lspconfig.gopls.setup {
-  settings = {
-    gopls = {
-      analyses = {
-        unusedparams = true,
-      },
-      staticcheck = true,
-      gofumpt = true,
-    },
-  },
-}
